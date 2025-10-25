a7028f42177678e8006275749cf40a1c8568a383
root <root@DESKTOP-PP99CDD.localdomain>
2025-10-25 18:57:07 +0400

    Vehicle.h

---
+++ system _of_vehicles/Vehicle.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+class Vehicle
+{
+protected:
+	std::string model;
+	int year;
+	double price;
+public:
+	Vehicle();
+	Vehicle(std::string& model, int year, double price);
+	virtual ~Vehicle() = default;
+	virtual void displayInfo() const = 0;
+
+};
+
